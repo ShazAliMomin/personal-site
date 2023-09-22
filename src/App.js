fce62c7b10c123b6d1a83e0030e7522bd7cf9c41
ShazAliMomin <momin.shazali01@gmail.com>
2023-09-21 20:39:03 -0500

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+function Navbar() {
+  return (
+    <nav>
+      <h1 className="nav-logo">Shaz Ali Momin</h1>
+      <ul className="navigation">
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Contact Me</li>
+      </ul>
+      <ul className="social-links">
+        <li>
+          <a href="https://github.com/ShazAliMomin">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/shaz-ali-momin-514903211/">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
