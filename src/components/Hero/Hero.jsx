a82e68b3a37ea9526086163f25502b2f15766c26
ShazAliMomin <momin.shazali01@gmail.com>
2023-09-22 13:54:42 -0500

    Added Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Hero.css";
+import ProfileImg from "../../assets/Profile_Image/profilepic.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 function Hero() {
-  return <div className="hero-container"></div>;
+  return (
+    <div className="hero-container">
+      <div className="hero-info">
+        <div className="profile-img-background">
+          <img src={ProfileImg} alt="Shaz Ali Momin"></img>
+        </div>
+        <div className="intro-container">
+          <h1>Hi, I'm Shaz!</h1>
+          <p>Software Developer</p>
+          <button className="resume-button">Download Resume</button>
+        </div>
+      </div>
+      <div className="next-section-arrow">
+        <button>
+          <FontAwesomeIcon icon={faAngleDown} />
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
