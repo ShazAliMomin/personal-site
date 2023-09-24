51d4a72fd26eab04fbb9fbf2bed468f7d6935048
ShazAliMomin <momin.shazali01@gmail.com>
2023-09-23 20:40:53 -0500

    Added experience content sections

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import "./Experience.css";
 
 function Experience() {
-  return <div className="experience-container"></div>;
+  return (
+    <div className="experience-section">
+      <h1>Experience</h1>
+      <div className="experience-container">
+        <div className="education-container"></div>
+        <div className="work-container"></div>
+        <div className="extracirricular-container"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Experience;
