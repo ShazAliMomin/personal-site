e7b3d2da9f9b9f958d44926767357ee80c7a82a3
ShazAliMomin <momin.shazali01@gmail.com>
2023-09-23 20:54:50 -0500

    Setting up projects section

--- src/components/ProjectSection/Project.jsx
+++ src/components/ProjectSection/Project.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./Project.css";
 
 function Project() {
-  return <div className="project-container"></div>;
+  return (
+    <div className="project-section">
+      <h1>Projects</h1>
+      <div className="project-container">
+        <div className="starpath-card"></div>
+        <div className="infodrill-card"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Project;
