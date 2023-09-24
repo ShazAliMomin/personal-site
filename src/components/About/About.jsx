5a6948288e8df496ed2d5bdd6f34b7a0b6f2d053
ShazAliMomin <momin.shazali01@gmail.com>
2023-09-23 20:56:43 -0500

    setting up about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./About.css";
 
 function About() {
-  return <div className="about-container"></div>;
+  return (
+    <div className="about-container">
+      <h1>About Me</h1>
+    </div>
+  );
 }
 
 export default About;
