d582a07a684a09daf79fc1815ee0cc2184d636a2
ShazAliMomin <momin.shazali01@gmail.com>
2023-09-26 14:41:12 -0500

    Added two sections within about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,26 +5,48 @@ import UHLogo from "../../assets/UH_Red.png";
 function About() {
   return (
     <div className="about-container">
-      <div className="about-me-card">
-        <h1>About Me</h1>
-        <p></p>
+      <div className="about-card">
+        <h2 className="about-header">ABOUT ME</h2>
+        <div className="about-me">
+          <p>I am shaz</p>
+        </div>
       </div>
       <div className="education-skills-card">
+        <h2 className="education-header">EDUCATION</h2>
         <div className="education">
+          <div>
+            <p>
+              University of Houston{" "}
+              <span>
+                | <i>2019 - 2023</i>
+              </span>
+            </p>
+            <ul>
+              <li>B.S. in Computer Science</li>
+              <li>Minor in MIS</li>
+              <li>Minor in Mathematics</li>
+            </ul>
+          </div>
           <img className="uh-logo" src={UHLogo} alt="UH Logo"></img>
-          <p>
-            University of Houston{" "}
-            <span>
-              | <i>2019 - 2023</i>
-            </span>
-          </p>
-          <ul>
-            <li>B.S. in Computer Science</li>
-            <li>Minor in MIS</li>
-            <li>Minor in Mathematics</li>
-          </ul>
         </div>
-        <div className="skills"></div>
+        <h2 className="skills-header">SKILLS</h2>
+        <div className="skills">
+          <div className="languages">
+            <ul>
+              <li>Python</li>
+              <li>C++</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>ReactJS</li>
+              <li>Java</li>
+              <li>Github</li>
+              <li>Word</li>
+              <li>Excel</li>
+              <li>Powerpoint</li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
