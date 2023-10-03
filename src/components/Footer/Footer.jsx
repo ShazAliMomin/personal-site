8442cc33451c88dbe9a75b0fc15d364d0fdc1913
ShazAliMomin <momin.shazali01@gmail.com>
2023-10-02 19:19:06 -0500

    Updating footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReact, faCss3 } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-  return <div className="footer-container"></div>;
+  return (
+    <div className="footer-container">
+      <p>Built by Shaz Ali Momin using</p>
+      <div className="lang-logo">
+        <FontAwesomeIcon icon={faReact} />
+        <FontAwesomeIcon icon={faCss3} />
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
